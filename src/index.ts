7fbe5fbd528b7dce7afab10c649b493d66e7d65d
NotStark <YeahAmStark@Gmail.com>
2024-03-03 17:25:43 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosResponse } from "axios";
+
+interface InteractionData {
+    uuid: string;
+    interactionId: string;
+    promptId: string;
+    prompt: string;
+    id: string;
+    message: string;
+    profaned: string;
+    disclaimer: string;
+}
+
+interface ApiResponse {
+    code: number;
+    data?: InteractionData;
+    HttpStatus: 'OK' | 'UNAUTHORIZED';
+    timestamp: number;
+    status: 'success' | 'error';
+    error?: string;
+    message?: string;
+}
+
+export default class Krutrim {
+    private readonly authorization: string;
+    private readonly headers: Record<string, string>;
+
+    constructor(authorization: string) {
+        this.authorization = authorization;
+        this.headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Authorization": authorization,
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+        };
+    }
+
+    async chatCompletion(prompt: string, intersectionId: string | null = null): Promise<ApiResponse | Error> {
+        const payload: { message: string, intersectionId?: string } = { message: prompt };
+
+        if (intersectionId) {
+            payload.intersectionId = intersectionId;
+        }
+
+        try {
+            const res: AxiosResponse = await axios.post("https://chat.olakrutrim.com/chatapp/chat", payload, {
+                headers: this.headers
+            });
+            return res.data;
+
+        } catch (error) {
+            console.error('Error occurred during chat completion:', error);
+            throw error;
+        }
+    }
+}
---
+++ test.ts
@@ -0,0 +1,9 @@
+import Krutrim from "./src/index";
+
+const auth = ""
+
+const kru = new Krutrim(auth)
+
+    ; (async () => {
+        console.log((await kru.chatCompletion("Hi")));
+    })()
